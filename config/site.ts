a742b32c8cd751fed8f6ef002ef374462229b9fc
sepehrgr7 <agent.1378@gmail.com>
2025-08-06 11:32:42 +0330

    Remove navbar item

--- config/site.ts
+++ config/site.ts
@@ -24,10 +24,6 @@ export const siteConfig = {
       label: 'تماس با ما',
       href: '/contact',
     },
-    {
-      label: 'خرید و مشاوره',
-      href: '/buy-form',
-    },
   ],
   navMenuItems: [
     {
