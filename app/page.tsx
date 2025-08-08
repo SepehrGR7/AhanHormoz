06eb421a0d01361c420d22dee4276d2221aa547b
sepehrgr7 <agent.1378@gmail.com>
2025-08-08 13:45:02 +0330

    Update homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
+import Hero from '@/components/hero'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <section className='flex flex-col items-center justify-center gap-4 py-8 md:py-10'>
-      Ahan Hormoz
-      <Image src='/1.png' alt='e33' width={4000} height={4000} />
-      <Image src='/2.jpeg' alt='cp2077' width={4000} height={4000} />
+    <section className='flex flex-col items-center justify-center gap-4 py-8 bg-stone-50 dark:bg-red-400'>
+      <Hero />
+      {/* <Image src='/1.png' alt='e33' width={4000} height={4000} /> */}
+      {/* <Image src='/2.jpeg' alt='cp2077' width={4000} height={4000} /> */}
     </section>
   )
 }
