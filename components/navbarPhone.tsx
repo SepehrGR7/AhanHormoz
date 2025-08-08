918f19bb9bac83bf26c386d055e4cd1af3ed17aa
Sepehr Gorgini <agent.1378@gmail.com>
2025-08-09 01:41:51 +0330

    Remove background

--- components/navbarPhone.tsx
+++ components/navbarPhone.tsx
@@ -2,11 +2,11 @@ import { Headset } from 'lucide-react'
 
 export default function NavbarPhone() {
   return (
-    <div className='text-gray-500  flex items-center gap-2'>
-      <Headset size={32} className='bg-gray-300 p-1 rounded-full' />
+    <div className='flex items-center gap-2 text-gray-500'>
+      <Headset size={32} className='rounded-full' />
       <div className='flex flex-col items-center dark:text-gray-300'>
         <p className='text-xs'>مشاوره و خرید</p>
-        <p className=''>
+        <p>
           <span className='text-xs'>076</span>-
           <span className='font-semibold'>33380664</span>
         </p>
