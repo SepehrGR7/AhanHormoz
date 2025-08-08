44ed0c1466d9670374f08e4816714b237dfe9ab9
sepehrgr7 <agent.1378@gmail.com>
2025-08-08 14:27:35 +0330

    Add component

---
+++ components/navmenu.tsx
@@ -0,0 +1,50 @@
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarSub,
+  MenubarSubContent,
+  MenubarSubTrigger,
+  MenubarTrigger,
+} from '@/components/ui/menubar'
+import { ChevronDown } from 'lucide-react'
+import { link as linkStyles } from '@heroui/theme'
+import NextLink from 'next/link'
+import clsx from 'clsx'
+
+export default function NavMenu() {
+  return (
+    <Menubar>
+      <MenubarMenu>
+        <MenubarTrigger className='text-xs font-normal'>
+          قیمت محصولات
+          <ChevronDown className='h-4 w-4 mr-0.5' />
+        </MenubarTrigger>
+        <MenubarContent>
+          <MenubarSub>
+            <MenubarSubTrigger>
+              <NextLink
+                href='/rebar'
+                className={clsx(
+                  linkStyles({ color: 'foreground' }),
+                  'data-[active=true]:text-primary data-[active=true]:font-medium text-xs',
+                )}
+              >
+                قیمت میلگرد
+              </NextLink>
+            </MenubarSubTrigger>
+            <MenubarSubContent>
+              <MenubarItem>میلگرد ساده</MenubarItem>
+              <MenubarItem>میلگرد ساده</MenubarItem>
+              <MenubarItem>میلگرد ساده</MenubarItem>
+              <MenubarItem>میلگرد ساده</MenubarItem>
+            </MenubarSubContent>
+          </MenubarSub>
+        </MenubarContent>
+      </MenubarMenu>
+    </Menubar>
+  )
+}
