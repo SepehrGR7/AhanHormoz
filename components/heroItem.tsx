aec099e2f20bd98d4b1839e2d4772d5bd7096580
sepehrgr7 <agent.1378@gmail.com>
2025-08-08 14:34:17 +0330

    Update

--- components/heroItem.tsx
+++ components/heroItem.tsx
@@ -9,26 +9,24 @@ export default function HeroItem({
   icon: string
 }) {
   return (
-    <div>
-      <Card>
-        <CardHeader className='flex gap-2'>
-          <i className={`${icon} text-[36px]`}></i>
-          <h3 className='font-medium'>قیمت {label}</h3>
-        </CardHeader>
-        <Divider />
-        <CardBody className='text-sm pr-5 flex flex-row'>
-          <ul className='flex flex-col items-start'>
-            <li>میلگرد ساده</li>
-            <li>میلگرد غیر ساده</li>
-            <li>میلگرد خیلی ساده</li>
-          </ul>
-          <ul className='flex flex-col items-start'>
-            <li>میلگرد ساده</li>
-            <li>میلگرد غیر ساده</li>
-            <li>میلگرد خیلی ساده</li>
-          </ul>
-        </CardBody>
-      </Card>
-    </div>
+    <Card className='max-w-lg'>
+      <CardHeader className='flex gap-2'>
+        <i className={`${icon} text-[36px]`}></i>
+        <h3 className='font-medium'>قیمت {label}</h3>
+      </CardHeader>
+      <Divider />
+      <CardBody className='text-sm pr-5 flex flex-row'>
+        <ul className='flex flex-col items-start'>
+          <li>میلگرد ساده</li>
+          <li>میلگرد غیر ساده</li>
+          <li>میلگرد خیلی ساده</li>
+        </ul>
+        <ul className='flex flex-col items-start'>
+          <li>میلگرد ساده</li>
+          <li>میلگرد غیر ساده</li>
+          <li>میلگرد خیلی ساده</li>
+        </ul>
+      </CardBody>
+    </Card>
   )
 }
