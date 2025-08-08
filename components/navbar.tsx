4027a99e73fa890dd3154a27dc74b1d2ba7ad08f
sepehrgr7 <agent.1378@gmail.com>
2025-08-08 14:29:23 +0330

    Update

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -84,6 +84,7 @@ export const Navbar = () => {
 
       <NavbarMenu>
         <div className='mx-4 mt-2 flex flex-col gap-4'>
+          <NavMenu />
           {siteConfig.navMenuItems.map((item, index) => (
             <NavbarMenuItem key={`${item}-${index}`}>
               <Link color={'foreground'} href='#' size='lg'>
