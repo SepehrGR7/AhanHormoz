86dd20bc209acd934f5f7ae86f7558e55696400d
Sepehr Gorgini <agent.1378@gmail.com>
2025-08-09 20:10:10 +0330

    Adjust icon

--- components/navbarPhone.tsx
+++ components/navbarPhone.tsx
@@ -1,15 +1,18 @@
 import { Headset } from 'lucide-react'
+import Link from 'next/link'
 
 export default function NavbarPhone() {
   return (
     <div className='flex items-center gap-2 text-gray-500'>
       <Headset size={32} className='rounded-full' />
-      <div className='flex flex-col items-center dark:text-gray-300'>
-        <p className='text-xs'>مشاوره و خرید</p>
-        <p>
-          <span className='text-xs'>076</span>-
-          <span className='font-semibold'>33380664</span>
-        </p>
+      <div className=' dark:text-gray-300'>
+        <Link href='tel:07633380664' className='flex flex-col items-center'>
+          <p className='text-sm font-bold'>مشاوره و خرید</p>
+          <p>
+            <span className='text-sm'>076</span>-
+            <span className='font-bold'>33380664</span>
+          </p>
+        </Link>
       </div>
     </div>
   )
