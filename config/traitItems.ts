c1525fcae102ec364ce6c9839872f1f14e432224
sepehrgr7 <agent.1378@gmail.com>
2025-08-10 22:01:02 +0330

    Add traits section

---
+++ config/traitItems.ts
@@ -0,0 +1,34 @@
+export const traitItems: {
+  id: number
+  title: string
+  icon: 'Coins' | 'Banknote' | 'ShieldCheck' | 'PhoneCall'
+  description: string
+}[] = [
+  {
+    id: 4,
+    title: 'مشاوره پیش از خرید',
+    icon: 'PhoneCall',
+    description:
+      'ارتباط با مشاوران فروش جهت مشاوره و خرید از طریق تماس با شماره 9999-076 جهت اطمینان خاطر از روند خرید.',
+  },
+  {
+    id: 1,
+    title: 'تضمین بهترین قیمت',
+    icon: 'Coins',
+    description: 'تضمین بهترین قیمت برای محصولات دیجیتال',
+  },
+  {
+    id: 2,
+    title: 'فروش با ضمانتنامه بانکی و LC',
+    icon: 'Banknote',
+    description:
+      'براساس مفاد پیش فاکتور و توافق حاصله، تسویه حساب به صورت نقدی یا اعتباری انجام می‌شود.',
+  },
+  {
+    id: 3,
+    title: 'ضمانت کیفیت کالا',
+    icon: 'ShieldCheck',
+    description:
+      'کالا به دقت فراوان توسط کارشناسان کنترل کیفیت بازرسی شده و در صورت تایید، مجوز بارگیری صادر می‌شود.',
+  },
+]
