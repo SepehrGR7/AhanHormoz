8fdd534f0c90ea44c0cc0215880d39fe69ceb00c
sepehrgr7 <agent.1378@gmail.com>
2025-08-10 20:48:41 +0330

    Change phone number

--- components/navbarPhone.tsx
+++ components/navbarPhone.tsx
@@ -6,11 +6,11 @@ export default function NavbarPhone() {
     <div className='flex items-center gap-2 text-gray-500'>
       <Headset size={32} className='rounded-full' />
       <div className=' dark:text-gray-300'>
-        <Link href='tel:07633380664' className='flex flex-col items-center'>
+        <Link href='tel:0769999' className='flex flex-col items-center'>
           <p className='text-sm'>مشاوره و خرید</p>
           <p>
             <span className='text-sm'>076</span>-
-            <span className='font-bold'>33380664</span>
+            <span className='font-bold'>9999</span>
           </p>
         </Link>
       </div>
