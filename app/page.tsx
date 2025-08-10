9c13b03c393c696cb9f241202c8fcc6313192704
sepehrgr7 <agent.1378@gmail.com>
2025-08-10 14:07:46 +0330

    Add traits section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from '@/components/hero'
+import Traits from '@/components/traits'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <section className='flex flex-col items-center justify-center gap-4 py-8 bg-stone-50 dark:bg-red-400'>
+    <section className='flex flex-col items-center justify-center dark:bg-stone-950'>
       <Hero />
+      <Traits />
       {/* <Image src='/1.png' alt='e33' width={4000} height={4000} /> */}
       {/* <Image src='/2.jpeg' alt='cp2077' width={4000} height={4000} /> */}
     </section>
