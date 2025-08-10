2fff961622ece50d6d61d114b615c072790cdadf
sepehrgr7 <agent.1378@gmail.com>
2025-08-10 14:09:10 +0330

    Update hero section

--- components/hero.tsx
+++ components/hero.tsx
@@ -4,9 +4,9 @@ import { title } from './primitives'
 
 export default function Hero() {
   return (
-    <section className='text-center'>
+    <section className='bg-gray-100 text-center px-4 py-10 w-full'>
       <h2 className={title({ color: 'blue' })}>قیمت روز آهن آلات</h2>
-      <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-6 mt-10'>
+      <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-6 mt-12  '>
         {siteConfig.heroItems.map(item => (
           <HeroItem key={item.name} label={item.label} icon={item.icon} />
         ))}
--- components/heroItem.tsx
+++ components/heroItem.tsx
@@ -1,5 +1,4 @@
 import { Card, CardBody, CardHeader } from '@heroui/card'
-import { Divider } from '@heroui/divider'
 
 export default function HeroItem({
   label,
@@ -9,19 +8,20 @@ export default function HeroItem({
   icon: string
 }) {
   return (
-    <Card className='max-w-lg'>
+    <Card className='max-w-lg relative overflow-visible'>
       <CardHeader className='flex gap-2'>
-        <i className={`${icon} text-[36px]`}></i>
-        <h3 className='font-medium'>قیمت {label}</h3>
+        <div className='bg-white border shadow dark:bg-gray-700 flex gap-2 items-center py-1 px-2 rounded-full absolute -top-5 z-50 text-md'>
+          <i className={`${icon} text-[32px]`}></i>
+          <h3 className='font-medium'>قیمت {label}</h3>
+        </div>
       </CardHeader>
-      <Divider />
-      <CardBody className='text-sm pr-5 flex flex-row'>
-        <ul className='flex flex-col items-start'>
+      <CardBody className='text-sm pr-5 grid grid-cols-2 gap-10'>
+        <ul className='flex flex-col items-start gap-1.5'>
           <li>میلگرد ساده</li>
           <li>میلگرد غیر ساده</li>
           <li>میلگرد خیلی ساده</li>
         </ul>
-        <ul className='flex flex-col items-start'>
+        <ul className='flex flex-col items-start gap-1'>
           <li>میلگرد ساده</li>
           <li>میلگرد غیر ساده</li>
           <li>میلگرد خیلی ساده</li>
