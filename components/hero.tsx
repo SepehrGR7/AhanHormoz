64d7808bf866a2647130fb2775e293c019d80b0f
sepehrgr7 <agent.1378@gmail.com>
2025-08-10 21:07:52 +0330

    Change background color

--- components/hero.tsx
+++ components/hero.tsx
@@ -4,7 +4,7 @@ import { title } from './primitives'
 
 export default function Hero() {
   return (
-    <section className='bg-gray-100 dark:bg-stone-950 text-center px-4 py-10 w-full'>
+    <section className='text-center px-4 py-10 w-full'>
       <h2 className={title({ color: 'blue' })}>قیمت روز آهن آلات</h2>
       <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-10 md:gap-4 mt-12'>
         {siteConfig.heroItems.map(item => (
