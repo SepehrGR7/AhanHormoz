8ca9faef1bdf0bafbc2fabce446b0bbbfe47f51a
sepehrgr7 <agent.1378@gmail.com>
2025-08-11 20:48:06 +0330

    Change height

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
         )}
       >
         <Providers themeProps={{ attribute: 'class', defaultTheme: 'dark' }}>
-          <div className='relative flex flex-col h-screen'>
+          <div className='relative flex flex-col'>
             <Navbar />
             <main className='container mx-auto max-w-7xl md:px-6 flex-grow'>
               {children}
