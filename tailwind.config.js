a9fad311877e53846ef1be2d3881ddbf72c6899f
sepehrgr7 <agent.1378@gmail.com>
2025-10-02 16:36:27 +0330

    Add custom effects for gradient

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,32 @@
-import {heroui} from "@heroui/theme"
+import { heroui } from '@heroui/theme'
 
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: [
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
+    './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-sans)"],
-        mono: ["var(--font-mono)"],
+        sans: ['var(--font-sans)'],
+        mono: ['var(--font-mono)'],
+      },
+      keyframes: {
+        gradient: {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        },
+      },
+      animation: {
+        gradient: 'gradient 8s linear infinite',
       },
     },
   },
-  darkMode: "class",
+  darkMode: 'class',
   plugins: [heroui()],
 }
 
-module.exports = config;
+module.exports = config
